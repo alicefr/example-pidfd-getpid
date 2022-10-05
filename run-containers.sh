58b60109fa096ff0af615b8a81bf90c64d442e96
Alice Frosi <afrosi@redhat.com>
2022-10-05 15:23:22 +0200

    Launch the containers

---
+++ run-containers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+set -x
+
+docker run -ti -d --name pr-helper \
+  --pid host \
+  --privileged \
+  pr-helper
+
+docker run --name qemu --security-opt label=disable \
+	--device /dev/sdb:/dev/sdb \
+	--device /dev/kvm:/dev/kvm \
+	-u root:kvm -td qemu
+pid=$(docker inspect --format "{{.State.Pid}}" qemu)
+docker exec -ti pr-helper connector -pid=$pid -fd=3
