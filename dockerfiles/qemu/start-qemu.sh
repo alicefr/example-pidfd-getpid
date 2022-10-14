a9a80a7c9a226c90eb363e0c4a9c631610862695
Alice Frosi <afrosi@redhat.com>
2022-10-14 11:00:31 +0200

    Fix socket path for the pr-helper

--- dockerfiles/qemu/start-qemu.sh
+++ dockerfiles/qemu/start-qemu.sh
@@ -10,7 +10,7 @@ set -x
 	-nodefaults \
 	-m 1024 \
         -device virtio-scsi \
-        -object pr-manager-helper,id=helper0,path=/proxy.sock \
+        -object pr-manager-helper,id=helper0,path=/var/run/qemu-pr-helper.sock \
         -blockdev node-name=hd,driver=raw,file.driver=host_device,file.filename=/dev/sdb,file.pr-manager=helper0 \
         -device scsi-block,drive=hd \
 	-hda /disk/disk.img
